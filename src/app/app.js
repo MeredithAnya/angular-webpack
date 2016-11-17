f0a07b7a1c506cd8cec61bcc9bb66477ee0bad1a
Meredith <meredith@getsentry.com>
2016-11-17 13:18:51 -0800

    use raven CDN

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,6 @@
 import angular from 'angular';
-var Raven = require('raven-js');
 import '../style/app.css';
 
-Raven
-  .config('https://7a852c96ff0d4600938c724153f0bc2a@sentry.io/107523')
-  .addPlugin(require('raven-js/plugins/angular'), angular)
-  .install();
 
 let app = () => {
   return {
@@ -23,7 +18,7 @@ class AppCtrl {
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, ['ngRaven'])
+angular.module(MODULE_NAME, [])
   .directive('app', app)
   .controller('AppCtrl', AppCtrll)
 
